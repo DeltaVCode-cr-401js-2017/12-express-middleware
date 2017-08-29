76d7a7930a62003478f8e989496d51c8f23f3fa2
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-08-29 09:51:34 -0500

    initial setup

---
+++ lab-beeksma/server.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const express  = require('express');
+const debug = require('debug')('app:server')
+
+const PORT = process.env.PORT || 3000;
+const app = module.exports = express();
+
+if (!module.parent) {
+  app.listen(PORT, () => {
+    debug( `Listening on ${PORT}`);
+  });
+}

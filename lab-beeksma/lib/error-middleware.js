b57dc785c71cac7e1f688e254e05ebd14266a271
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-08-29 10:25:11 -0500

    error handler and GET 500 route

--- lab-beeksma/lib/error-middleware.js
+++ lab-beeksma/lib/error-middleware.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const createError = require('http-errors');
+const debug = require('debug')('app:error-middleware');
+
+module.exports = function (err,req,res, next) {
+  console.error();(err.messge);
+  debug(err.status);
+
+  if(err.status) {
+    debug('user error');
+  }
+  else{
+    debug('server error');
+    err = new createError.InternalServerError(err.message);
+  }
+
+  res.status(err.status).send(err.name);
+  next();
+};
--- lab-beeksma/server.js
+++ lab-beeksma/server.js
@@ -6,6 +6,12 @@ const debug = require('debug')('app:server');
 const PORT = process.env.PORT || 3000;
 const app = module.exports = express();
 
+app.get('/500', (req,res,next) => {
+  next(new Error('boom'));
+});
+
+app.use(require('./lib/error-middleware'));
+
 if (!module.parent) {
   app.listen(PORT, () => {
     debug( `Listening on ${PORT}`);
